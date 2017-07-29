cbc668fce8e6bbc6019dcab9230bf4aa17a5ff20
arcadeperfect <alexander.harding@gmail.com>
2017-07-29 13:16:55 -0400

    nettime works

--- netTime.py
+++ netTime.py
@@ -1,9 +1,34 @@
+import datetime
 import ntplib
-from time import ctime
+import time
+import os
 
-c = ntplib.NTPClient()
-response = c.request('europe.pool.ntp.org', version=3)
+client = ntplib.NTPClient()
+response = client.request('pool.ntp.org', version=3)
+
 #print response.offset
 
+'''
+to deliberately set the system time wrong for testing purposes:
+
+sudo date -s "Thu Aug  9 21:31:26 UTC 2012"
+'''
+
+'''
+print "system time \n"
+print datetime.datetime.now()
+print "\n"
+
+print "ntplib time \n"
 print response.tx_time
-print ctime(response.tx_time)
+print time.ctime(response.tx_time)
+print '\n'
+'''
+
+### Set system time
+
+def setTime():
+	print 'setting system time'
+	os.system('date ' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time)))
+	
+setTime()
