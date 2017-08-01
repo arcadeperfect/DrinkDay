7af524794ce4e062eccf79763a827dfcbb5b84d7
Alexander Harding <alexharding@Prismo.local>
2017-08-01 01:31:24 -0400

    can now download any new files in a bucket

--- gcs/gcs.py
+++ gcs/gcs.py
@@ -1,12 +1,21 @@
-# https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-python
-# https://googlecloudplatform.github.io/google-cloud-python/stable/storage/client.html
-# https://googlecloudplatform.github.io/google-cloud-python/stable/storage/blobs.html
+"""
+https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-python
+https://googlecloudplatform.github.io/google-cloud-python/stable/storage/client.html
+https://googlecloudplatform.github.io/google-cloud-python/stable/storage/blobs.html
+https://cloud.google.com/storage/docs/object-basics#download
+"""
+
 
 from google.cloud import storage
+from os import listdir
 
-client = storage.Client()
-bucketName = "drinkday_images"
-b = client.get_bucket(bucketName)
 
-for i in b.list_blobs():
-	print i
+def downloadNew(bucketName='drinkday_images', imagePath='./img'):
+    bucket = storage.Client().get_bucket(bucketName)
+    for blob in bucket.list_blobs():
+        if not blob.name in listdir(imagePath):
+            print 'downloading %s' % blob.name
+            blob.download_to_filename('./img/%s' % blob.name)
+
+
+downloadNew()
